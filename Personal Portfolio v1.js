2290016b1176eef02056c7c85c83676ec660e065
Tyler <100461106+NakortheIslani@users.noreply.github.com>
2022-03-31 19:33:57 -0800

    Add files via upload

--- Personal Portfolio v1.js
+++ Personal Portfolio v1.js
@@ -1,16 +1,21 @@
 var words = [
-    'Resourcful',
-    'a Quick Study',
-    'Reliable',
-    'Learning Everyday',
-    'Punctual',
-    'Fun to Work With',
-    'a Hard Worker',
-    'Awesome',
-    'a Problem Solver',
-    'Legendary',
-    'Alien-esque',
-    'An Array of Adjectives',
+    'Resourcful.',
+    'a Quick Study.',
+    'Reliable.',
+    'Learning Everyday.',
+    'Punctual.',
+    'Fun to Work With.',
+    'a Hard Worker.',
+    'Awesome.',
+    'a Problem Solver.',
+    'Legendary.',
+    'Alien.',
+    'An Array of Positive Adjectives.',
+    'a Meat Popsicle.',
+    'Spartacus.',
+    'Batman.',
+    'Ironman.',
+    'Pickle Rick!!!'
 ];
 
 function randomWord() {
